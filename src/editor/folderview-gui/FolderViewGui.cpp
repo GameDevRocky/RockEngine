bca21598a9045193b6b49cec6450edd7b6ca490f
Rocklyn Clarke <rocklyn.clarke1@gmail.com>
2025-11-21 22:47:05 -0500

    Added back button

--- src/editor/folderview-gui/FolderViewGui.cpp
+++ src/editor/folderview-gui/FolderViewGui.cpp
@@ -1,17 +1,42 @@
 #include "FolderViewGui.hpp"
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QLabel>
 #include <QFileInfo>
+#include <QWidget>
 #include "EditorUtils.hpp"
 
-FolderViewGui::FolderViewGui(QWidget* parent) : QWidget(parent), currentPath("") {
-    QVBoxLayout* layout = new QVBoxLayout(this);
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->setSpacing(4);
+FolderViewGui::FolderViewGui(QWidget* parent) : QWidget(parent), currentPath("C:/Users/rockl/Coding Projects/RockEngine"), projectDirectory("C:/Users/rockl/Coding Projects/RockEngine") {
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    mainLayout->setSpacing(0);
+
+    // Create navigation bar (fixed height)
+    QWidget* navBar = new QWidget(this);
+    navBar->setFixedHeight(36);
+    navBar->setStyleSheet("background-color: #2d2d2d; border-bottom: 1px solid #454545;");
+    QHBoxLayout* navLayout = new QHBoxLayout(navBar);
+    navLayout->setContentsMargins(1,1,1,1);
+    navLayout->setSpacing(4);
+
+    // Back button with arrow
+    backButton = new QPushButton("◀", this);
+    backButton->setFixedSize(28, 28);
+    backButton->setFlat(true);
+    backButton->setToolTip("Go to parent directory");
+    backButton->setEnabled(false);
+
+    connect(backButton, &QPushButton::clicked, this, &FolderViewGui::GoBack);
+
+    navLayout->addWidget(backButton);
+    navLayout->addStretch();
+    navBar->setLayout(navLayout);
+
+    mainLayout->addWidget(navBar);
 
     // Create file system model
     model = new QFileSystemModel(this);
-    model->setRootPath("");
+    model->setRootPath(projectDirectory);
     model->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
     model->setIconProvider(new EditorUtils::CustomIconProvider());
     gridView = new QListView(this);
@@ -22,6 +47,8 @@ FolderViewGui::FolderViewGui(QWidget* parent) : QWidget(parent), currentPath("")
     gridView->setIconSize(QSize(64, 64));
     gridView->setWrapping(true);
     gridView->setUniformItemSizes(true);
+    gridView->setGridSize(QSize(100, 110));  // Fixed cell size: 100px wide, 110px tall
+    gridView->setTextElideMode(Qt::ElideRight);  // Add "..." to long filenames
 
     gridView->setDragEnabled(true);                    
     gridView->setAcceptDrops(false);                  
@@ -30,8 +57,8 @@ FolderViewGui::FolderViewGui(QWidget* parent) : QWidget(parent), currentPath("")
     gridView->setEditTriggers(QAbstractItemView::NoEditTriggers);
     gridView->setSelectionMode(QAbstractItemView::SingleSelection);
 
-    layout->addWidget(gridView);
-    setLayout(layout);
+    mainLayout->addWidget(gridView);
+    setLayout(mainLayout);
 
     connect(gridView, &QListView::doubleClicked, this, [this](const QModelIndex& index) {
         if (model->isDir(index)) {
@@ -48,6 +75,16 @@ void FolderViewGui::Init() {
     setMinimumWidth(400);
 }
 
+void FolderViewGui::SetProjectDirectory(const std::string& projectDir) {
+    projectDirectory = QString::fromStdString(projectDir);
+    directoryHistory.clear();
+    directoryHistory.push(projectDirectory);
+    currentPath = projectDirectory;
+    QModelIndex rootIndex = model->index(projectDirectory);
+    gridView->setRootIndex(rootIndex);
+    UpdateBackButtonState();
+}
+
 void FolderViewGui::Navigate(const std::string& filepath) {
     QString path = QString::fromStdString(filepath);
     
@@ -56,8 +93,34 @@ void FolderViewGui::Navigate(const std::string& filepath) {
     if (!fileInfo.isDir())
         path = fileInfo.dir().absolutePath();
     
-    // Set the root index to display contents
-    currentPath = path;
+    // Add to history if different from current
+    if (path != currentPath) {
+        directoryHistory.push(currentPath);
+        currentPath = path;
+    }
+    
     QModelIndex rootIndex = model->index(path);
     gridView->setRootIndex(rootIndex);
+    UpdateBackButtonState();
+}
+
+void FolderViewGui::GoBack() {
+    if (directoryHistory.isEmpty())
+        return;
+
+    QString previousPath = directoryHistory.pop();
+    currentPath = previousPath;
+    
+    QModelIndex rootIndex = model->index(previousPath);
+    gridView->setRootIndex(rootIndex);
+    UpdateBackButtonState();
+}
+
+void FolderViewGui::UpdateBackButtonState() {
+    // Disable back button when at project directory
+    if (!projectDirectory.isEmpty()) {
+        backButton->setEnabled(currentPath != projectDirectory && !directoryHistory.isEmpty());
+    } else {
+        backButton->setEnabled(!directoryHistory.isEmpty());
+    }
 }
--- src/editor/folderview-gui/FolderViewGui.hpp
+++ src/editor/folderview-gui/FolderViewGui.hpp
@@ -2,7 +2,9 @@
 #include <QWidget>
 #include <QListView>
 #include <QFileSystemModel>
+#include <QPushButton>
 #include <QString>
+#include <QStack>
 
 class FolderViewGui : public QWidget {
     Q_OBJECT
@@ -14,12 +16,18 @@ public:
     }
     void Init();
     void Navigate(const std::string& filepath);
+    void SetProjectDirectory(const std::string& projectDir);
 
 private:
     explicit FolderViewGui(QWidget* parent = nullptr);
     ~FolderViewGui() override = default;
+    void GoBack();
+    void UpdateBackButtonState();
 
     QListView* gridView = nullptr;
     QFileSystemModel* model = nullptr;
+    QPushButton* backButton = nullptr;
     QString currentPath;
+    QString projectDirectory;
+    QStack<QString> directoryHistory;
 };
